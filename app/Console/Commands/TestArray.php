678027e648499a65cb419c19ac5fd5da309a2805
石晓农 <xiaonong.shi@wenba100.com>
2019-01-10 17:30:16 +0800

    错题本测试生产学情报告sql

--- app/Console/Commands/TestArray.php
+++ app/Console/Commands/TestArray.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class TestArray extends Command
 {
@@ -39,8 +40,40 @@ class TestArray extends Command
     {
         $this->info("测试开始");
         $startTime = microtime(true);
-
+        $this->test();
         $endTime = microtime(true);
         $this->info("测试结束 --- 共计用时" . ($endTime - $startTime) . "秒");
+    }
+
+    private function test()
+    {
+        $result = [];
+        DB::table('mistake_note')
+        ->select('user_id','grade_id','subject_id','point2_name',
+        DB::raw('GROUP_CONCAT(difficult) as difficult'),DB::raw('GROUP_CONCAT(mistake_cause) as mistake_cause'),
+        DB::raw('count(*) AS num'))
+            ->whereIn('grade_id',[2,3])
+            ->whereIn('subject_id',[2,4,5])
+            ->where('is_delete',0)
+            ->groupBy('user_id', 'grade_id', 'subject_id','point2_name')
+            ->having('num','>=',10)
+            ->orderBy('num','grade_id','subject_id')
+            ->chunk(10,function ($mistake_note) use (&$result) {
+                foreach ($mistake_note as $item){
+                    if(($item->grade_id==2 && $item->subject_id==2)
+                    || ($item->grade_id ==3 && in_array($item->subject_id ,[2,4,5]))) {
+                        $result[$item->user_id . '-' . $item->grade_id . '-' . $item->subject_id.'-'.$item->point2_name] = [
+                            'user_id' => $item->user_id,
+                            'grade_id' => $item->grade_id,
+                            'subject_id' => $item->subject_id,
+                            'point2_name' => $item->point2_name,
+                            'difficult' => array_count_values(explode(',', $item->difficult)),
+                            'mistake_cause' => array_count_values(explode(',', $item->mistake_cause))
+                        ];
+                    }
+
+                }
+            });
+        dump($result);
     }
 }

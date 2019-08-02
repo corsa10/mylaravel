51979f8e20a42d6822e2fe4162ff42fecfd62e36
corsa10 <xiaonongshi@gmail.com>
2019-08-02 19:34:32 +0800

    修改composer源地址；添加helper目录

--- app/Console/Commands/TestArray.php
+++ app/Console/Commands/TestArray.php
@@ -41,42 +41,15 @@ class TestArray extends Command
         $this->info("测试开始");
         $startTime = microtime(true);
 
-        $sql = "replace into wechat_wenba_rel(id,nick_name,uid)values";
-        $studentList = [11111,22222,33333,44444];
-        $wechatList = [
-            ['Id'=>12,'NickName'=>'学霸君uid:11111'],
-            ['Id'=>13,'NickName'=>'学霸君uid:22222'],
-            ['Id'=>14,'NickName'=>'学霸君uid:33333'],
-            ['Id'=>15,'NickName'=>'学霸君UID:44444'],
-            ['Id'=>16,'NickName'=>'学霸君uid:11111'],
-            ['Id'=>17,'NickName'=>'学霸君uid:22222'],
-            ['Id'=>18,'NickName'=>'学霸君uid:22222'],
-        ];
-        foreach($wechatList as $k=>$item){
-            foreach($studentList as $key=>$value) {
-                if(false !== mb_strrpos($item['NickName'],$value)) {
-                    $sql .= "(" . $item['Id'] . ",'" . addslashes($item['NickName']) . "',".$value."),";
-                    continue 2;
-                }
-                dump($key,$value,$k,$item);
-            }
-        }
-        $sql=rtrim($sql,',').";";
-        dump($sql);
-        DB::connection('mysql')->insert($sql);
+        $date =  mktime(0,0,0,date('m'),date('d'));
+        $month = mktime(0, 0, 0, date("m"), date("d") - 1, date("Y"));
+
+        $endTime = mktime(23, 59, 59, date('m', strtotime(date('Y-m',$month))) + 1, 0);
+
+
+        dump(date('Y-m-d',$month),date('Y-m-d:H:i:s',$endTime));
         $endTime = microtime(true);
         $this->info("测试结束 --- 共计用时" . ($endTime - $startTime) . "秒");
     }
-
-    /**
-     * [test description]
-     * @param  Array $params [description]
-     * @return Array         [description]
-     */
-    public function test(array $params) :array
-    {
-        return [];
-    }
-
 
 }
